1bb7de2f153d81c1c084cf9132046442662af4d2
brahmananda854 <54118136+brahmananda854@users.noreply.github.com>
2024-11-26 09:03:47 +0000

    learning datatypes in JS

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553;
+let accountEmail ="xxxxxx@gmail.com";
+var accountPassword = "1234";
+accountCity="BBSR";
+let accountState;
+// accountId =2; // const value cannot be changed 
+accountEmail = "hc@hc.com"
+accountPassword="4567"
+accountCity = "Cuttack"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+/* 
+    Prefer not to use var because of issue in block scope and fuctional scope
+*/
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,31 @@
+"use strict" ; // treat all JS code as newer version of JS not older version
+//alert(3 + 3) // donot run alert as its node engine not browser
+
+//DATATYPES
+
+//1. Premitives
+//2. Reference Types
+//********************* */
+//number===>2 to the power 53
+//bigInt 
+//string==> "" 
+//boolean ==> true or false
+//null ==> standalone value represntation of null not indefind but empty value
+//undefind ==> when value is not defind to a variable
+//symbol ==> To findout uniqueness in value 
+
+  
+let a=10; // number
+let b="Brahma" //string
+let isAvailable = true //boolean
+let c= null // null
+let d; //undefind
+console.log(typeof a);
+console.log(typeof null);
+console.log(typeof isAvailable);
+console.log(typeof undefined);
+
+//2. Refrence type
+/******************* */
+//Object
+//Array
