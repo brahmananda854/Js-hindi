91ef8328cb14566f41dc4cca61da53d05d856546
brahmananda854 <54118136+brahmananda854@users.noreply.github.com>
2024-11-26 10:47:05 +0000

    datatype full

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -3,29 +3,57 @@
 
 //DATATYPES
 
-//1. Premitives
-//2. Reference Types
+//1. Premitives (Copy of value is given to work on)
+//2. Reference Types or non premitives
 //********************* */
-//number===>2 to the power 53
-//bigInt 
-//string==> "" 
-//boolean ==> true or false
-//null ==> standalone value represntation of null not indefind but empty value
-//undefind ==> when value is not defind to a variable
-//symbol ==> To findout uniqueness in value 
+//1. Premitives  (7 types) 
+//    i.number===>2 to the power 53
+//    ii.bigInt 
+//   iii.string==> "" 
+//    iv.boolean ==> true or false
+//     v.null ==> standalone value represntation of null not indefind but empty value
+//    vi.undefind ==> when value is not defind to a variable
+//   vii.symbol ==> To findout uniqueness in value 
 
-  
-let a=10; // number
-let b="Brahma" //string
-let isAvailable = true //boolean
-let c= null // null
-let d; //undefind
-console.log(typeof a);
-console.log(typeof null);
-console.log(typeof isAvailable);
-console.log(typeof undefined);
+  //let a=10; // number
+//let b="Brahma" //string
+//let isAvailable = true //boolean
+//let c= null // null
+//let d; //undefind
+//let id = Symbol('123');
+//let anotherId = Symbol('123')  // though both id and anotherId are having same values but they are diffrent
+//console.log(id==anotherId);
+//let bigInt = 1234323n
 
-//2. Refrence type
+// console.log(typeof a);
+// console.log(typeof null);
+// console.log(typeof isAvailable);
+// console.log(typeof undefined);
+
+//2. Refrence type(Non premitives)
 /******************* */
-//Object
-//Array
+// Memory address Reference of variable is given
+
+// i.  Object
+
+let myObj = 
+{
+    name:"Brahma",
+    id:"1",
+    addr:"BBSR"
+}
+
+console.log(typeof myObj);
+
+
+// ii. Array
+
+let myArr = ["Shaktiman", "Xero"];
+console.log(typeof myArr);
+// iii.Functions
+
+let myFun = function(){
+    return (2+2)
+}
+//console.log(myFun());
+console.log(typeof myFun);
--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -6,18 +6,18 @@
 // console.log(2!=1);
 
 //comparision and equality check are diffrent
-console.log("2" > 1);
-console.log("02" > 1);
+// console.log("2" > 1);
+// console.log("02" > 1);
 // always compare with same datatypes
-console.log(null > 0)
-console.log(null == 0)
-console.log(null >= 0)
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
 
-console.log(undefind > 0)
-console.log(undefined == 0)
-console.log(undefind >= 0)
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
 //Avoid above type of comparision
 
 //strict check
 
-console.log("2" === 2);
+//console.log("2" === 2);
