76dae9fdc35ab669e9a35a1d6129116203bf09f3
brahmananda854 <54118136+brahmananda854@users.noreply.github.com>
2024-11-26 09:59:40 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+//comparision and equality check are diffrent
+console.log("2" > 1);
+console.log("02" > 1);
+// always compare with same datatypes
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+console.log(undefind > 0)
+console.log(undefined == 0)
+console.log(undefind >= 0)
+//Avoid above type of comparision
+
+//strict check
+
+console.log("2" === 2);
