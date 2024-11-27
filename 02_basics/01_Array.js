79444d62edc098a388a42f6a453fb44ee39ffea8
brahmananda854 <54118136+brahmananda854@users.noreply.github.com>
2024-11-27 15:21:26 +0000

    Array learn more part-02

--- 02_basics/01_Array.js
+++ 02_basics/01_Array.js
@@ -26,10 +26,44 @@ let myArr2 = new Array(1,2,3,4,5,'Brahma');
 
 const myarrrr = [1,2,3,4,5,6];
 const mynarr = myarrrr.slice(1,3);
-console.log("A", myarrrr);
+// console.log("A", myarrrr);
 const mynarr2 = myarrrr.splice(1,3);
-console.log(mynarr);  // dont include last character
-console.log(mynarr2); // includes last character
-console.log("B",  myarrrr);
+// console.log(mynarr);  // dont include last character
+// console.log(mynarr2); // includes last character
+// console.log("B",  myarrrr);
+
+// ++++++++++++++++++++++++++++++++++++++=ARRAY Part2 ++++++++++++++++++++++++++
+const marvel_heros=['ironman','superman','Saktiman'];
+const dc_heros=['ironman1','superman1','Saktiman1'];
+//marvel_heros.push(dc_heros);  //Push do a deep copy 
+// console.log(marvel_heros);
+//const allHeros = marvel_heros.concat(dc_heros); //concat do a shallow copy
+// console.log(marvel_heros);
+//console.log(allHeros);
+
+//spread operator
+
+const allNewHeros = [...marvel_heros,...dc_heros]
+//console.log(allNewHeros);
+
+const anotherArray = [1,2,3,4,5,[3,4,5],6,7,[8,9],3,4,6];  //mearges all arrays into one array 
+let real_anotherArray = anotherArray.flat(Infinity);
+//console.log(real_anotherArray);
+
+// When we get data we receive in any format like object or nodeList or string then to make it loop in Array
+
+console.log(Array.isArray("Brahma")); // its false means its a string 
+console.log(Array.from("Brahma"));
+console.log(Array.isArray({name:"Dipun"}));  // to make a object to Array we give wheather Key needs to be array or value 
+myObj = {name:"Dipun",age:18};
+console.log(Array.from(Object.keys(myObj)));
+console.log(Array.from(Object.values(myObj)));
+
+const score1= 100;
+const score2= 200;
+const score3= 300;
+// console.log(Array.of(score1,score2,score3));  it accumulates all the data dypes and make it to array
+
+
 
 
