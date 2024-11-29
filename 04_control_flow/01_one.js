705f54e18d55fdceae355ef5568c4fe42301287d
brahmananda854 <54118136+brahmananda854@users.noreply.github.com>
2024-11-29 12:22:52 +0000

    Switch

---
+++ 04_control_flow/01_one.js
@@ -0,0 +1,131 @@
+//if
+
+const isUserLoggedIn=true;  //assignment operator
+const tempreture = 41;
+if (isUserLoggedIn) {   //comparision <,>,<=,>=,== ,!=,===(strict equal)
+    //Run if true
+}
+// if (tempreture == 41) {
+//     console.log("execute !!!");
+    
+// } else {
+//     console.log("dont Execute !");
+
+// }
+
+// const score =100;
+// if (score>50) {
+//     const power ='fly';
+//     console.log(`you have power to ${power}`);
+    
+// }
+
+//console.log(`you have power to ${power}`); // throught an error due to block scope for const power
+
+//++++++++++Sort hand notation++++++++++++
+const balance = 1000;
+// if (balance>500) console.log("test");
+//it assumes thats a block scope there in code
+
+// if (balance<500) {
+//     console.log("a");
+    
+// }else if (balance<900) {
+//     console.log("b");
+    
+// }else if (balance==1000) {
+//     console.log("c");
+    
+//  }else{
+//     console.log("d");
+    
+//  }
+
+let userLoggedIn = true;
+const debitCard = true;
+const loggedInFromGoogle = false;
+const loggedInfromEmail = true;
+
+// if (userLoggedIn && debitCard) {
+//     log("Buy course !!!")
+// }
+
+if(loggedInFromGoogle || loggedInfromEmail){
+    console.log("User logged IN !!!!!!");
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
