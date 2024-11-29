e02b47090aeef7788d51aba6b283888c8abf16eb
brahmananda854 <54118136+brahmananda854@users.noreply.github.com>
2024-11-29 13:31:24 +0000

    while and do while

---
+++ 05_Iteration.js/02_whileloop.js
@@ -0,0 +1,19 @@
+// let i =0;
+// while (i<=10) {
+//     console.log(`${i}`);
+//     i = i+2;
+// }
+
+// let myArray  =["Shaktiman", "Batman", "Superman"]
+// let arr =0;
+// while (arr < myArray.length-1) {
+//     console.log(`${myArray[arr]}`)
+//     arr++;
+// }
+
+let score =11;
+do {
+    console.log(`${score}`);
+    score++;
+    
+} while (score<=10);
